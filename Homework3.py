75abb6c0264dff8877df9ac7303de0aa313d7bab
BessonNM <bnm_777@mail.ru>
2024-07-16 00:15:53 +0500

    Add files via upload

--- Homework3.py
+++ Homework3.py
@@ -1,8 +1,19 @@
+# первоначальный вариант:
 name = 'Natalia'
 print('Name:', name)
 age = 47
 print('Age:', age)
 new_age = age+1
 print('New Age:', new_age)
+is_student = True
+print('Is Student:', is_student)
+
+# исправленный вариант:
+name = 'Natalia'
+print('Name:', name)
+age = 47
+print('Age:', age)
+age = age+2
+print('New Age:', age)
 is_student = True
 print('Is Student:', is_student)
